60db561e2323ed186dd2e38be09899d07cdb8eed
Barbie <barbarajoyjones@gmail.com>
2011-01-21 15:19:54 -0500

    initial commit

---
+++ src/com/example/android_tea/TeaTime.java
@@ -0,0 +1,13 @@
+package com.example.android_tea;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TeaTime extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
